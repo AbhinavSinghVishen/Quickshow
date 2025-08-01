9d12c6a4a5644e57efaaf7d0e0560943e1dab0b3
Abhinav Vishen <av.abhinavvishen@gmail.com>
2025-08-01 21:33:57 +0530

    First commit

---
+++ client/src/main.jsx
@@ -0,0 +1,85 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Movies from './pages/Movies.jsx'
+import MovieDetails from './pages/MovieDetails.jsx'
+import SeatLayout from './pages/SeatLayout.jsx'
+import MyBookings from './pages/MyBookings.jsx'
+import Favourite from './pages/Favourite.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { ClerkProvider } from '@clerk/clerk-react'
+import Layout from './pages/admin/Layout.jsx'
+import Dashboard from './pages/admin/Dashboard.jsx'
+import AddShows from './pages/admin/AddShows.jsx'
+import ListShows from './pages/admin/ListShows.jsx'
+import ListBookings from './pages/admin/ListBookings.jsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/movies',
+        element: <Movies />
+      },
+      {
+        path: '/movies/:id',
+        element: <MovieDetails />
+      },
+      {
+        path: '/movies/:id/:date',
+        element: <SeatLayout />
+      },
+      {
+        path: '/my-bookings',
+        element: <MyBookings />
+      },
+      {
+        path: '/favourite',
+        element: <Favourite />
+      },
+      {
+        path: '/admin/*', //now because of this wild card any route containing admin will show layout even if that path is not defined 
+        element: <Layout />,
+        children:[
+          {
+            // path: '/',
+            index: true,
+            element: <Dashboard/>
+          },
+          {
+            path: 'add-shows',
+            element: <AddShows/>
+          },
+          {
+            path: 'list-shows',
+            element: <ListShows/>
+          },
+          {
+            path: 'list-bookings',
+            element: <ListBookings/>
+          },
+        ]
+      },
+    ]
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <RouterProvider router={router} />
+  </ClerkProvider>
+)
